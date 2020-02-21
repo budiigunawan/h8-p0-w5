d3472f95246b8851c58ded724b3622d91c6d3b9a
Budi Gunawan <budiindra.g@gmail.com>
2020-02-21 20:30:20 +0700

    menyelesaikan 1.js

--- goodluck-orion-fox/1.js
+++ goodluck-orion-fox/1.js
@@ -9,10 +9,55 @@ Contoh ada pada test case.
 [RULE]
 - Buatlah pseudocode!
 - Dilarang menggunakan built-in function kecuali .push()
+
+SET "targetArr" with array of string
+SET "title" with string
+SET "hasil" with empty array
+SET "titik" without value
+
+FOR "t" = 0 - "t" < "title".length
+  IF "title"[t] IS EQUAL TO '.' THEN "titik" IS 'true'
+  ENDIF
+ENDFOR
+
+IF "titik" IS 'true' THEN
+  FOR "i" = 0 - "i" < "targeArr".length
+    SET "temp" TO "title" + "targetArr"[i]
+    PUSH "temp" TO "hasil"
+  ENDFOR
+ELSE
+  FOR "i" = 0 - "i" < "targeArr".length
+    SET "temp" TO "targetArr"[i] + "title"
+    PUSH "temp" TO "hasil"
+  ENDFOR
+ENDIF
+///waktu 9 menit 5 detik
 */
 
 function titleGivers(targetArr, title) {
-  // your code here..
+  var hasil = []
+  var titik
+
+  for (var t = 0; t < title.length; t++) {
+    if (title[t] === '.') {
+      titik = true
+    }
+  }
+
+  if (titik == true) {
+    for (i = 0; i < targetArr.length; i++) {
+      var temp = `${title} ${targetArr[i]}`
+      hasil.push(temp)
+    }
+  }
+  else {
+    for (i = 0; i < targetArr.length; i++) {
+      var temp = `${targetArr[i]} ${title}`
+      hasil.push(temp)
+    }
+  }
+
+  return hasil
 }
 
 console.log(titleGivers(['Awtian Akbar', 'Kobayakawa Sena', 'Yoichi Hiruma'], 'Mr.'));
