17277bd9fac67f51f2f3ed9f2ed01f1e81b4fc6f
Budi Gunawan <budiindra.g@gmail.com>
2020-02-21 21:41:42 +0700

    menyelesaikan 5.js

--- goodluck-orion-fox/5.js
+++ goodluck-orion-fox/5.js
@@ -21,12 +21,56 @@
  *
  * Rules:
  *  - Tidak boleh menggunakan built-in function array. .map, .filter, .reduce, .sort
+ * 
+ * waktu 20 menit 44 detik
  */
-​
+
 function groupStage(teams) {
-  // Insert your code here
+  var hasil = {
+    '1' : {},
+    '2' : {},
+    '3' : {},
+    '4' : {}
+  }  
+
+  ///[['team',score],['team', score]]
+  var arrTemp = []
+  for (var i = 0; i < teams.length; i++) {
+    arrTemp[i] = []
+    var score = 0
+    
+    for (var j = 0; j < teams[i].matches.length; j++) {
+      if (teams[i].matches[j].status === 'win') {
+        score += 3
+      }
+      else {
+        score -= 1
+      }
+    }
+
+    arrTemp[i].push(teams[i].name, score)
+  }
+
+  ///sort rangking
+  for (var a = 1; a < arrTemp.length; a++) {
+    for (var b = 0; b < a; b++) {
+      if (arrTemp[a][1] > arrTemp[b][1]) {
+        var tukar = arrTemp[a]
+        arrTemp[a] = arrTemp[b]
+        arrTemp[b] = tukar
+      }
+    }
+  }
+
+  ///masukkin ke hasil
+  for (var h = 0; h < arrTemp.length; h++) {
+    hasil[String(h+1)].team = arrTemp[h][0]
+    hasil[String(h+1)].score = arrTemp[h][1]
+  }
+
+  return hasil
 }
-​
+
 var group1 = [
   {
     name: 'Evos',
@@ -97,31 +141,28 @@ var group1 = [
     ]
   }
 ]
-​
 console.log(groupStage(group1))
-​
-/**
- * {
- *  '1': {
- *  team: 'EVOS',
- *  score: 9
- * },
- * '2': {
- *  team: 'Alter Ego',
- *  score: 5
- * },
- * '3': {
- *  team: 'Onic',
- *  score: 5
- * },
- * '4': {
- *  team: 'RRQ',
- *  score: -3
- * }
- * }
- */
-​
-​
+// /**
+//  * {
+//  *  '1': {
+//  *  team: 'EVOS',
+//  *  score: 9
+//  * },
+//  * '2': {
+//  *  team: 'Alter Ego',
+//  *  score: 5
+//  * },
+//  * '3': {
+//  *  team: 'Onic',
+//  *  score: 5
+//  * },
+//  * '4': {
+//  *  team: 'RRQ',
+//  *  score: -3
+//  * }
+//  * }
+//  */
+
 var group2 = [
   {
     name: 'Evos',
@@ -193,9 +234,9 @@ var group2 = [
   }
 ]
 console.log(groupStage(group2))
-/**
- * { '1': { team: 'RRQ', score: 9 },
-  '2': { team: 'ONIC', score: 5 },
-  '3': { team: 'Alter Ego', score: 1 },
-  '4': { team: 'Evos', score: -3 } }
-**/
+// /**
+//  * { '1': { team: 'RRQ', score: 9 },
+//   '2': { team: 'ONIC', score: 5 },
+//   '3': { team: 'Alter Ego', score: 1 },
+//   '4': { team: 'Evos', score: -3 } }
+// **/
