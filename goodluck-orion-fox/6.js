974649c79e46afc98068430350e6120a721815f5
Budi Gunawan <budiindra.g@gmail.com>
2020-02-22 14:30:06 +0700

    membuat 6.js

--- goodluck-orion-fox/6.js
+++ goodluck-orion-fox/6.js
@@ -6,15 +6,37 @@ cobalah kerjakan soal ini dengan modular function
 Rules : 
 Dilarang menggunakan built-in function selain .push()
 */
+///waktu 8 menit 15 detik
 
+///untuk ngecek angka prima atau bukan
 function angkaPrima(angka) {
-    // your code here
+    if (angka > 1) {
+        var prima = true
+        for (var i = 2; i < angka; i++) {
+            if (angka % i == 0) {
+                prima = false
+            }
+        }
+    }
+    else {
+        prima = false
+    }
+
+    return prima
 }
 
 function angkaPrimaRange(num) {
-    // your code here
+    var hasil = []
+
+    for (var i = 0; i < num; i++) {
+        if (angkaPrima(i) == true) {
+            hasil.push(i)
+        }
+    }
+
+    return hasil
 }
 
 // TEST CASES
-console.log(angkaPrimaRange(6)); // [ 1, 2, 3, 5 ]
-console.log(angkaPrimaRange(33)); // [ 1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31 ]
+console.log(angkaPrimaRange(6)); // [ 2, 3, 5 ]
+console.log(angkaPrimaRange(33)); // [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31 ]
